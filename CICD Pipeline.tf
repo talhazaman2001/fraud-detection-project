2197483a2dc0430560941cb528c842a2d098459f
Unitedsection <unitedsection@gmail.com>
2024-09-26 11:03:31 +0100

    Update CICD Pipeline.tf

--- CICD Pipeline.tf
+++ CICD Pipeline.tf
@@ -182,7 +182,7 @@ resource "aws_codepipeline" "fraud_detection_pipeline" {
       category        = "Deploy"
       owner           = "AWS"
       provider        = "ECS"
-      version         = "1"
+      version         = "2"
       input_artifacts = ["build_output"]
 
       configuration = {
